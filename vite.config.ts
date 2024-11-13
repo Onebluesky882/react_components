b15eadc41adf20e0c923528b84d5fb728210cb4f
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-11-13 18:02:36 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "onebluesky882.github.io/react_api_form/",
+  base: "/react_api_form/",
 });
